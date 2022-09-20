9f509acaa390b4ef1cf566ecdaf34cb63a3f1da6
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-09-20 11:16:56 +0800

    修改登录

--- cvat-ui/src/components/login-page/login-page.tsx
+++ cvat-ui/src/components/login-page/login-page.tsx
@@ -14,6 +14,7 @@ import Button from 'antd/lib/button';
 import Divider from 'antd/lib/divider';
 import FooterDrawer from 'components/login-page/intel-footer-drawer';
 
+import consts from 'consts';
 import LoginForm, { LoginData } from './login-form';
 import { OpenVINOIcon } from 'icons';
 
@@ -85,6 +86,7 @@ function LoginPageComponent(props: LoginPageComponentProps & RouteComponentProps
                     </Col>
                 </Row>
             </Content>
+            <FooterDrawer />
         </Layout>
     );
 }
