1ad61e09198ba823af47c58a0a368cac82fd208e
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-10-06 18:24:53 +0800

    服务器

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -322,9 +322,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = os.getenv('TZ', 'Etc/UTC')
+# TIME_ZONE = os.getenv('TZ', 'Etc/UTC')
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
